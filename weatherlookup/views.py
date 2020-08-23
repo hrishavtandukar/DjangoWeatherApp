99bd703a2efcc7aac76936ce30b4f9d3e179080c
hrishavtandukar <hrishav.tandukar@gmail.com>
2020-08-23 21:07:53 +0545

    Two API integrated on same page

--- weatherlookup/views.py
+++ weatherlookup/views.py
@@ -2,7 +2,33 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request, 'home.html', {})
+	import json
+	import requests
+
+	api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=504CB3FD-B442-47D4-A1E0-BC30B0F3CCED")
+	api_request_nepal = requests.get("https://nepal-weather-api.herokuapp.com/api/?place=all")
+
+	try:
+		api = json.loads(api_request.content)
+		api_nepal = json.loads(api_request_nepal.content)
+
+	except Exception as e:
+		api = "Error in calling API...."
+
+	return render(request, 'home.html', {'api':api, 'api_nepal':api_nepal})
 
 def about_us(request):
 	return render(request, 'about_us.html', {})
+
+# def api_request_for_nepal():
+# 	import json
+# 	import requests
+# 	api_request_nepal = requests.get("https://nepal-weather-api.herokuapp.com/api/?place=all")
+
+# 	try:
+# 		api_nepal = json.loads(api_request_nepal.content)
+
+# 	except Exception as e:
+# 		api_nepal = "Error in calling API...."
+
+# 	return render(request, 'home.html', {'api_nepal':api_nepal})
