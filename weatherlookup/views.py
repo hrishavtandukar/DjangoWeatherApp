c8f132d9e150cda79998806051476f96e9439e59
hrishavtandukar <hrishav.tandukar@gmail.com>
2020-08-27 22:07:41 +0545

    Colors automatically change

--- weatherlookup/views.py
+++ weatherlookup/views.py
@@ -15,7 +15,31 @@ def home(request):
 	except Exception as e:
 		api = "Error in calling API...."
 
-	return render(request, 'home.html', {'api':api, 'api_nepal':api_nepal})
+	if api[0]['Category']['Name'] == "Good":
+		category_description = "(0-50) Air quality is considered satisfactory."
+		category_color = "good"
+
+	elif api[0]['Category']['Name'] == "Moderate":
+		category_description = "(51-100) Air quality is acceptable."
+		category_color = "moderate"
+
+	elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
+		category_description = "(101-150) Although general public is not likely to be affected at this AQI range, people with lung disease, elderly people and children are at a greatest risk point."
+		category_color = "usg"
+
+	elif api[0]['Category']['Name'] == "Unhealthy":
+		category_description = "(151-200) Everyone may begin to experience health effects."
+		category_color = "unhealthy"
+
+	elif api[0]['Category']['Name'] == "Very Unhealthy":
+		category_description = "(201-300) Health alert."
+		category_color = "veryunhealthy"
+
+	elif api[0]['Category']['Name'] == "Hazardous":
+		category_description = "(300-500) Health warning of emergency condition."
+		category_color = "hazardous"
+	
+	return render(request, 'home.html', {'api':api, 'api_nepal':api_nepal, 'category_description':category_description, 'category_color':category_color})
 
 def about_us(request):
 	return render(request, 'about_us.html', {})
